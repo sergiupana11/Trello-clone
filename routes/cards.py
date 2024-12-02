9085909d568e696ac3cc7e48d19d7d95cf75643f
Sergiu Pana <74061574+sergiupana11@users.noreply.github.com>
2024-12-02 22:06:09 +0200

    Update cards.py

--- routes/cards.py
+++ routes/cards.py
@@ -1,19 +1,13 @@
 from flask import Blueprint, request, jsonify
 from bson.objectid import ObjectId
-from marshmallow import ValidationError
 from app import mongo
-from schemas.card_schema import CardSchema
 
 card_routes = Blueprint('card_routes', __name__)
-card_schema = CardSchema()
 
+# Create a new card in a list
 @card_routes.post('/<list_id>/cards')
 def create_card(list_id):
-    try:
-        data = card_schema.load(request.json)
-    except ValidationError as err:
-        return jsonify({'errors': err.messages}), 400
-    
+    data = request.json
     card_list = mongo.db.lists.find_one({'_id': ObjectId(list_id)})
     if not card_list:
         return jsonify({'error': 'List not found'}), 404
@@ -27,3 +21,54 @@ def create_card(list_id):
         {'$push': {'cards': card_id}}
     )
     return jsonify({'id': card_id, 'title': data['title'], 'description': data.get('description', '')})
+
+# Get all cards in a list
+@card_routes.get('/<list_id>/cards')
+def get_cards_in_list(list_id):
+    card_list = mongo.db.lists.find_one({'_id': ObjectId(list_id)})
+    if not card_list:
+        return jsonify({'error': 'List not found'}), 404
+    
+    card_ids = card_list.get('cards', [])
+    cards = list(mongo.db.cards.find({'_id': {'$in': [ObjectId(id) for id in card_ids]}}))
+    for card in cards:
+        card['_id'] = str(card['_id'])
+    return jsonify(cards)
+
+# Get details of a card
+@card_routes.get('/<card_id>')
+def get_card(card_id):
+    card = mongo.db.cards.find_one({'_id': ObjectId(card_id)})
+    if not card:
+        return jsonify({'error': 'Card not found'}), 404
+    card['_id'] = str(card['_id'])
+    return jsonify(card)
+
+# Update a card's title and/or description
+@card_routes.put('/<card_id>')
+def update_card(card_id):
+    data = request.json
+    updates = {}
+    if 'title' in data:
+        updates['title'] = data['title']
+    if 'description' in data:
+        updates['description'] = data['description']
+    
+    mongo.db.cards.update_one({'_id': ObjectId(card_id)}, {'$set': updates})
+    return jsonify({'message': 'Card updated successfully'})
+
+# Delete a card
+@card_routes.delete('/<card_id>')
+def delete_card(card_id):
+    card = mongo.db.cards.find_one({'_id': ObjectId(card_id)})
+    if not card:
+        return jsonify({'error': 'Card not found'}), 404
+    
+    # Remove card from its list
+    mongo.db.lists.update_one(
+        {'cards': str(card_id)},
+        {'$pull': {'cards': str(card_id)}}
+    )
+    # Delete the card
+    mongo.db.cards.delete_one({'_id': ObjectId(card_id)})
+    return jsonify({'message': 'Card deleted successfully'})
